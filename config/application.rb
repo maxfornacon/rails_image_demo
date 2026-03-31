fdc5d75b8feb0341d5bb36fc613488e124029d1f
Maximilian Fornaçon <maximilian.fornacon@gmail.com>
2026-03-31 16:21:33 +0200

    ui: add translation for missing attachment error message

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module PictrsDemo
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.default_locale = :de
   end
 end
